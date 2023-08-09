d89aec22aeb39e65906391172b1415b3c5800411
Carson Halter <84420466+cah311@users.noreply.github.com>
2023-08-09 14:08:20 -0600

    set up nav bar w light mode

--- client/src/scenes/layout/index.js
+++ client/src/scenes/layout/index.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { UseSelector } from "react-redux";
+import Navbar from "components/Navbar";
 
 const layout = () => {
-  return <div>layout</div>;
+  return (
+    <Box width="100%" height="100%">
+      <Box>
+        <Navbar />
+        <Outlet />
+      </Box>
+    </Box>
+  );
 };
 
 export default layout;
